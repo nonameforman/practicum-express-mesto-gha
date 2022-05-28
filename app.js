8de2390eae16cc5906fa80ac4469d88cdfb54993
A Gordeev <nonameforman1@yandex.ru>
2022-05-28 18:48:34 +0300

    feat: 404 on not exist page

--- app.js
+++ app.js
@@ -18,6 +18,10 @@ app.use((req, res, next) => {
 app.use(usersRouter);
 app.use(cardsRouter);
 
+app.use((req, res) => {
+  res.status(404).send({ message: "Данной страницы не существует" });
+})
+
 async function main() {
   await mongoose.connect("mongodb://localhost:27017/mestodb", {
     useNewUrlParser: true,
